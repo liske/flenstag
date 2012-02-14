b5e915759ea9f820bf77f748186b94955caf8000
Thomas Liske <thomas@fiasko-nw.net>
2012-02-14 21:15:19 +0100

    Update example config file:  - remove auth stuff  - add ISO & focal length tagging

--- flenstag.conf.ex
+++ flenstag.conf.ex
@@ -1,14 +1,21 @@
-# Flickr API authentication stuff
-$flickr_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-$flickr_sec = 'XXXXXXXXXXXXXXXX';
-$flickr_auth_token = '';
-
-# Howto search for photos to be tagged?
+# Howto search for photos to be tagged? The user_id can be retrieved
+# using the Flickr API explorer:
+# http://www.flickr.com/services/api/explore/flickr.people.findByUsername
 $flickr_search_req = 'flickr.photos.search';
-$flickr_search_arg = {user_id => '61323542@N05'};
+$flickr_search_arg = {
+    user_id => '61323542@N05',
+    per_page => 500
+};
+
+# Add (min|max)_upload_date field to $flickr_search_arg?
+#  undef: just try to tag any we get (but not more than 500 pics)
+#  min  : update new uploads only (related to last flenstag call)
+#  max  : update old uploads only (related to last flenstag call)
+$flickr_search_upload_date = 'min';
+
 
 # Which EXIF fields should become tags?
-@exif_tags = qw(Model LensModel Lens);
+@exif_tags = qw(Model LensModel Lens ISO FocalLength);
 
 # Regex pattern converting lens strings.
 my $lens = {
@@ -19,5 +26,7 @@ my $lens = {
 # Regex patterns, used to transform EXIF fields into tag string.
 %format_tags = (
     Lens => $lens,
-    LensModel => $lens
+    LensModel => $lens,
+    ISO => {'(\d+)' => 'ISO $1'},
+    FocalLength => {'(\d+)(\.\d+)? mm' => '$1 mm'},
 );
